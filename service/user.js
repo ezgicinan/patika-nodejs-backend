ea06e0a8b8916b5bb4896c957ece2476192450f0
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-11-22 01:22:38 +0300

    Existing user control added.

--- service/user.js
+++ service/user.js
@@ -4,16 +4,24 @@ const mongooseUser = require('../model/user')
 async function createUser(userParams) {
     const {username,email,password} = userParams;
     try {
+        // Check if a user with the same email exists
+        const existingUser = await mongooseUser.findOne({ email });
+
+        if (existingUser) {
+            console.log('#UserCreate: User already exists with this email');
+            return { success: false, object: 'User with this email already exists' };
+        }
+
         const hashedPassword = bcrypt.hashSync(password,10);
         const newUser = new mongooseUser({
             username,
             email,
             password:hashedPassword
         })
-        newUser.save();
-        return true;
+        await newUser.save();
+        return { success: true, object: 'User created successfully.' };
     } catch (error) {
-        console.log(e);
+        console.log('Error #UserCreate: ', error);
     }
 }
 
