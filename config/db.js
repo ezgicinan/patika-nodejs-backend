e5180ada3f03ba11908ca7e1aed3a76fb4426e17
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-11-22 01:22:10 +0300

    Comment deleted.

--- config/db.js
+++ config/db.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose')
 async function connectionDB() {
     try {
         console.log("before mongoose");
-        //await mongoose.connect('mongodb+srv://ezgicinan35:16NAvE4murrTMZnt@newmind.gkxlr.mongodb.net/backend_patika?retryWrites=true&w=majority');
-        await mongoose.connect('mongodb+srv://ezgicinan35:16NAvE4murrTMZnt@newmind.gkxlr.mongodb.net/');
+        await mongoose.connect('mongodb+srv://ezgicinan35:16NAvE4murrTMZnt@newmind.gkxlr.mongodb.net/backend_patika');
         console.log("Bağlandık.");
     } catch (e) {
-        console.log('mongoDBERR:', e);
+        console.log('Error #db-mongo:', e);
     }
 }
 
