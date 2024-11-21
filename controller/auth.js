f8183cadfb64f4d1a197cc5684fc974e6678d87d
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-11-22 01:24:23 +0300

    register operation updated.

--- controller/auth.js
+++ controller/auth.js
@@ -3,20 +3,33 @@ const authController = {
     login: async(req,res)=>{
         try{
             console.log(req.body.email,'email')
-            console.log(req.body.password,'password')
-            //res.status(200).send({response:req.body})
-            res.status(200).send({response:{}})
-        }catch(e){
-            console.log(e,'error')
+            res.status(200).send({response:req.body})
+        }catch(error){
+            console.log('Error #auth/login: ', error);
         }
     },
     register:async(req,res) => {
         try {
-            console.log('geldi');
             const response = await userService.createUser(req.body);
-            res.status(200).send({response:'success'})
+            
+            if(response.success == false) {
+                return res.status(400).json({
+                    success: response.success, 
+                    object:response.object,
+                    statusCode: 400,
+                    response:'User already exist.', 
+                    httpMessage:'BAD_REQUEST'})
+            }
+
+            res.status(201).json({
+                success: response.success, 
+                object:response.object, 
+                statusCode: 201, 
+                response:'User created successfully.', 
+                httpMessage:'CREATED'})
         } catch (error) {
-            console.log("authControl ERR: ", e);
+            console.log("Error #auth/register: ", error);
+            return { success: false, message: 'An error occurred during user creation' };
         }
     }
 }
